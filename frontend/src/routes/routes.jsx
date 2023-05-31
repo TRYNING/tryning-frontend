04806cf282efe3e4d9043a346a3e462585b7c8bf
Juan Peñalba <juantandil123@gmail.com>
2023-05-30 21:10:20 -0300

    rutas actualizadas

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Rutas() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
