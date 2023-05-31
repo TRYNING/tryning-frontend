bd75cb72040fe7b64c39d44312cfde37bf7a2bdd
Juan Peñalba <juantandil123@gmail.com>
2023-05-30 21:55:53 -0300

    fix bug rutas

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import Login from "../pages/Login/Login";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 export default function Rutas() {
   return (
