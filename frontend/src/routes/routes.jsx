92ee7f3422c732f87fe6debad4587c59aac1ad96
Juan Peñalba <juantandil123@gmail.com>
2023-05-31 15:37:11 -0300

    div sacado del componente rutas

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -4,13 +4,11 @@ import Login from "../pages/Login";
 
 export default function Rutas() {
   return (
-    <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-    </div>
   );
 }
