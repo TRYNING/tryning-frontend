2e96e4f0af0543098edb3543884f706b6cba76f7
Juan Peñalba <juantandil123@gmail.com>
2023-06-05 22:17:35 -0300

    a

--- frontend/src/context/AuthContextProvider.jsx
+++ frontend/src/context/AuthContextProvider.jsx
@@ -1,16 +1,26 @@
-
 import { createContext, useState } from "react";
 
-export const AuthContext = createContext({})
+export const AuthContext = createContext({});
 
-export function AuthContextProvider({children}) {
-    const [user,setUSer] = useState()
+export function AuthContextProvider({ children }) {
+  const [user, setUSer] = useState();
 
-    const googleSingUp = async () => {
-        console.log("pito")
-    }
-    
-    return <AuthContext.Provider value={{user, googleSingUp}}>
-        {children}
+  const googleSingUp = async () => {
+    console.log("pito");
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (usuario) => {
+      setUser(usuario);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, googleSingUp }}>
+      {children}
     </AuthContext.Provider>
+  );
 }
