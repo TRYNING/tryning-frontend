54b5377edf005002b175f6aba2f625f76389b2e1
Juan Peñalba <juantandil123@gmail.com>
2023-06-07 20:15:05 -0300

    quite lo de la prueba de login del home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,33 +1,14 @@
-import { useAuthContext } from "../../hooks/useAuthContext";
 import { Header } from "../../components/Header/Header";
-import { SignOutButton, SignUpButton } from "../../components/buttons/Buttons";
+
 import "../../components/buttons/Buttons.scss";
 import "./Home.scss";
 
 export default function Home() {
-  const { user } = useAuthContext();
-  console.log(user);
-
   return (
     <>
       <Header />
       <main>
         <h1>Home</h1>
-        <SignUpButton label="Registrarse con google" />
-        <section>
-          {user && (
-            <div className="user">
-              <h1>
-                Bienvenido <span>{user.displayName}</span>
-              </h1>
-              <div className="ph-mail">
-                <img src={user.photoURL} />
-                <p>{user.email}</p>
-              </div>
-              <SignOutButton label="Cerrar Sesion" />
-            </div>
-          )}
-        </section>
       </main>
     </>
   );
--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header/Header";
+import { SignUpButton } from "../../components/buttons/Buttons";
 import "./Login.scss";
 
 export default function Login() {
@@ -6,6 +7,7 @@ export default function Login() {
     <div>
       <Header />
       <h1>Login</h1>
+      <SignUpButton label="Registrarse con google" />
     </div>
   );
 }
