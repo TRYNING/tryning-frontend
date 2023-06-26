a0d1bde3c570a20054f5972e4378e2c6c92b843c
BenjaEspindola <alexisespind@example.com>
2023-06-26 20:04:52 -0300

    form details

--- src/pages/RegisterDetails/components/FormDetails.jsx
+++ src/pages/RegisterDetails/components/FormDetails.jsx
@@ -1,9 +1,31 @@
-export default function FormRegisterDetails() {
+export function FormRegisterDetails() {
     return (
-      <div className="Form-register-details-container">
-        <a href="/">volver</a>
-        <h1>Registrarse</h1>
-      </div>
+       <form className="Form-register-details-container" >
+      <label>Usuario</label>
+      <input
+        type="text"
+        placeholder="Ingresa tu usuario"
+      />
+      <label>Nombre</label>
+      <input
+        type="text"
+        placeholder="Ingresa tu nombre"
+        
+      />
+      <label>Apellido</label>
+      <input
+        type="text"
+        placeholder="Ingresa tu apellido"
+        
+      />
+      <button>Registrarse</button>
+      {/* <div className="error-container">
+        <p>{errorAuth && errorAuth}</p>
+      </div> */}
+      <div id="o">o</div>
+      {/* <button onClick={() => googleSignUp()}>Registrarse con Google</button> */}
+     
+    </form>
     );
   }
   
--- src/pages/RegisterDetails/index.jsx
+++ src/pages/RegisterDetails/index.jsx
@@ -1,10 +1,11 @@
-import { FormRegisterDetails } from "../RegisterDetails/"
+import { FormRegisterDetails } from "../RegisterDetails/components/FormDetails";
+
 export default function RegisterDetails() {
-    return (
-      <div className="Register-details-container">
-        <a href="/">volver</a>
-        <h1>Registrarse</h1>
-        <FormRegisterDetails />
-      </div>
-    );
-  }
+  return (
+    <div className="Register-details-container">
+      <a href="/">volver</a>
+      <h1>Tus datos</h1>
+      <FormRegisterDetails />
+    </div>
+  );
+}
