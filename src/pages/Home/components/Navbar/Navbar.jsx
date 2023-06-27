0b25f0c588c14ef10a3149029df5299d79695579
Benjagodeto1 <alexisespind@gmail.com>
2023-06-26 21:15:29 -0300

    nav

---
+++ src/pages/Home/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+export function Navbar() {
+  return (
+    <nav className="Navbar-container">
+      <div className="button">
+        <a href="/home">HOME</a>
+      </div>
+      <div className="button">
+        <a href="/rutinas">RUTINA</a>
+      </div>
+      <div className="button">
+        <a href="/perfil">PERFIL</a>
+      </div>
+    </nav>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
+import { Navbar } from "./components/Navbar/Navbar";
+
 export default function Home() {
   return (
     <div className="Home-container">
       <main>
         <h1>Home</h1>
       </main>
+      <Navbar />
     </div>
   );
 }
