02c64f2fc65e658b3875a68dc97d173f1aa7f256
Benjagodeto1 <alexisespind@gmail.com>
2023-07-05 13:25:51 -0300

    styles landing

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,7 +3,9 @@ import { FormRegister } from "./components/FormRegister/FormRegister";
 export default function Register() {
   return (
     <div className="Register-container">
-      <a href="/">volver</a>
+      <div className="logo">
+        <img src="src/pages/Landing/logo.png" />
+      </div>
       <h1>Registrarse</h1>
       <FormRegister />
     </div>
