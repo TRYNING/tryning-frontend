b4d153397d5ef40e0cccc00e88a44a80ca7edc3a
Yaco Recroa <yacorecroa@gmail.com>
2023-07-07 19:27:36 -0300

    m

--- src/pages/Register/components/FormDetailsExtra/FormDetailsExtra.jsx
+++ src/pages/Register/components/FormDetailsExtra/FormDetailsExtra.jsx
@@ -13,13 +13,13 @@ export function FormDetailsExtra({ prevStep }) {
   return (
     <form className="FormDetailsExtra-container" onSubmit={(e) => handleSubmit(e)}>
       <label>Fecha de nacimiento</label>
-      <input type="date" required />
+      <input type="month" required />
       <label>Peso corporal en kg</label>
       <input type="number" required />
       <label>Estatura en cm</label>
       <input type="number" required />
       <button>Registrate</button>
-      <button onClick={() => prevStep()}>Anteriori</button>
+      <button onClick={() => prevStep()}>Anterior</button>
     </form>
   );
 }
