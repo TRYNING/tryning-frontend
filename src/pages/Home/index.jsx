88b09caec6ed1ef2f488bb33b5b9e8b65316f201
Benjagodeto1 <alexisespind@gmail.com>
2023-07-08 22:38:11 -0300

    home hecho

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,18 @@
 import { Header } from "../../components/Header/Header";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="Home-container">
+      <Header />
       <main>
-        <Header />
+        <h1>Tu entrenador personalizado en línea</h1>
+        <p>¡Alcanza tus metas más rápido con un entrenador personal!</p>
+        <div className="buttons-container">
+        <Link to="/trainers" className="button-entrenador">
+          Buscar entrenador
+        </Link>
+        </div>
       </main>
     </div>
   );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,12 +4,14 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { RequireAuth, ExistUser } from "../components/ProtectRoutes";
+import Profile from "../pages/Profile";
 
 export default function Rutas() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/profile" element={<Profile />} />
         <Route
           path="/login"
           element={
