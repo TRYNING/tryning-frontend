41c2f2d771d3240f0e4a113d4d83cabed1625f34
Yaco Recroa <yacorecroa@gmail.com>
2023-07-11 19:22:40 -0300

    Merge branch 'desarrollo' of https://github.com/JuanPE44/tryning-frontend into desarrollo

--- src/pages/BecomeTrainer/index.jsx
+++ src/pages/BecomeTrainer/index.jsx
@@ -7,6 +7,7 @@ export default function BecomeTrainer() {
     <div className="BecomeTrainer-container">
       <Header />
       <Nav />
+      
       <main>
         <h1>Tu entrenador personalizado en línea</h1>
         <p>¡Alcanza tus metas más rápido con un entrenador personal!</p>
