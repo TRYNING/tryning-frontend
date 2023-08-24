ade646f923e11eb1e02fb702dbbee0502de3b863
Benjagodeto1 <alexisespind@gmail.com>
2023-08-24 19:42:59 -0300

    manu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,24 +13,37 @@ export function Header() {
 
   return (
     <header className="Header-container">
+      <div className="menu-icono" onClick={handleLogoClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+          <g id="_118_menu-04" data-name="118 menu-04">
+            <rect x="4" y="24" width="16" height="2" />
+            <rect x="4" y="6" width="24" height="2" />
+            <rect x="4" y="15" width="24" height="2" />
+          </g>
+        </svg>
+      </div>
       <section className="container-logo">
-        <img src={logo} alt="logo de tryning" onClick={handleLogoClick} />
+        <Link to="/">
+          <img src={logo} alt="logo de tryning" />
+        </Link>
       </section>
       <section className={`menu ${open ? "active" : ""}`} id="menu">
         <ul className="container-link">
-          <li className="link">
-            <Link to="/account">MI CUENTA</Link>
-          </li>
-          <li className="link">
-            <Link to="/about-us">NOSOTROS</Link>
-          </li>
-          <li className="link">
-            <Link to="/contact">CONTACTO</Link>
-          </li>
+          <Link to="/account" className="link">
+            <li>MI CUENTA</li>
+          </Link>
+          <Link to="/about-us" className="link">
+            <li>NOSOTROS</li>
+          </Link>
 
-          <li className="link trainer">
-            <Link to="/become-trainer">VOLVERSE ENTRENADOR</Link>
-          </li>
+          <Link to="/contact" className="link">
+            <li>CONTACTO</li>
+          </Link>
+
+          <Link to="/become-trainer" className="link trainer">
+            <li>VOLVERSE ENTRENADOR</li>
+          </Link>
+
           <li className="link logout" onClick={() => SignOut()}>
             CERRAR SESION
           </li>
