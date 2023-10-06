97acb04542805318f7cd86f977c41c9ab7dec5b5
Yaco Recroa <yacorecroa@gmail.com>
2023-10-06 18:08:25 -0300

    Funciona analytics con un moock

--- src/pages/user/Analytics/Componentes/CarruselAnalytics.jsx
+++ src/pages/user/Analytics/Componentes/CarruselAnalytics.jsx
@@ -3,14 +3,14 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';1
 
-export function  CarruselAnalytics({ejercicios}) {
+export function  CarruselAnalytics({data}) {
 
 
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 3,
+      slidesToShow: 1,
       slidesToScroll: 1,
       responsive: [
         {
@@ -37,29 +37,16 @@ export function  CarruselAnalytics({ejercicios}) {
         <div className='mb-5 mt '>
           <h2 className='text-center m-2 font-bold'>Analytics</h2>
           <Slider {...settings}>
-            <div>
-              <TablaAnalytics
-              title="SEMANA 1"
-            ejercicios={ejercicios}
-              
-              />
-            </div>
-            <div>
-            <TablaAnalytics
-            title="SEMANA 2"
-            ejercicios={ejercicios}/>
-            
-            </div>
-            <div>
-            <TablaAnalytics
-            title="SEMANA 3"
-            ejercicios={ejercicios}/>
-            </div>
-            <div>
-            <TablaAnalytics
-            title="SEMANA 4"
-            ejercicios={ejercicios}/>
-            </div>
+            {
+              data.map((semana, i) => {
+                return (
+                  <TablaAnalytics
+                  key={i}
+                    title={semana.title}
+                    ejercicios={semana.ejercicios}/>
+                )
+              })
+            }
             {/* Agrega más elementos div para más imágenes */}
           </Slider>
         </div>
